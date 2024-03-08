768b3cb2d5f3f4a5746074a47daa68100f858426
Linus Björklund <weedzcokie@gmail.com>
2024-03-08 21:12:07 +0100

    first!

---
+++ src/index.ts
@@ -0,0 +1,85 @@
+import { type ChildProcess, spawn } from "node:child_process";
+
+let cwd: undefined | string;
+
+const cmdsFromArgv: string[] = [];
+
+// Skip "bin" arguments (like `node` and the path to this script), assumes we run in "node" environment and
+// just skip the first 2 arguments :+1:
+for (const arg of process.argv.slice(2)) {
+    // Ignore flags and arguments
+    if (arg.startsWith("--")) {
+        const [argument, value] = arg.split("=", 2);
+        if (argument === "--cwd") {
+            cwd = value;
+        }
+    } else {
+        cmdsFromArgv.push(arg);
+    }
+}
+
+if (cmdsFromArgv.length !== 1) {
+    console.error("doin it wrong..");
+    process.exit(1);
+}
+
+const cmds: (string | string[])[] = JSON.parse(cmdsFromArgv[0]);
+// TODO: Validate `cmds`
+
+const childProcess: ChildProcess[] = [];
+
+for (let i = 0; i < cmds.length; ++i) {
+    const cmd = cmds[i];
+    if (Array.isArray(cmd)) {
+        spawnSubcommands(cmd, i);
+    } else {
+        spawnCommand(cmd, i);
+    }
+}
+
+process.on("SIGINT", async (code) => {
+    await Promise.allSettled(childProcess.map(child => {
+        return new Promise<void>((resolve) => {
+            child.once("exit", resolve);
+            child.kill(code);
+        });
+    }));
+    process.exit(0);
+});
+
+// TODO: Should probably use `yargs` instead
+function parseCommand(cmd: string) {
+    const argsIndex = cmd.indexOf(" ");
+    const command = argsIndex > 0 ? cmd.slice(0, argsIndex) : cmd;
+    const args = argsIndex > 0 ? cmd.slice(argsIndex + 1).split(" ") : undefined;
+    return { command, args };
+}
+
+function spawnCommand(commandStr: string, i: number) {
+    return new Promise<number>((resolve) => {
+        const { command, args } = parseCommand(commandStr);
+        const child = spawn(command, args, { cwd });
+        childProcess.push(child);
+        child.stdout.on("data", (data: Buffer) => {
+            console.log(`[${i}] [${new Date().toISOString()}]: ${data.toString("utf-8").trim()}`);
+        });
+        child.stderr.on("data", (data: Buffer) => {
+            console.warn(`[${i}] [${new Date().toISOString()}] (err): ${data.toString("utf-8").trim()}`);
+        });
+        child.on("exit", (code) => {
+            console.log(`[${i}] [${new Date().toISOString()}]: "${commandStr}" Exited with code ${code ?? "SIGINT"}`);
+            if (code === 0) {
+                resolve(0);
+            } else {
+                resolve(code || 0);
+            }
+        });
+    });
+}
+
+async function spawnSubcommands(subcommands: string[], i: number) {
+    for (const subcommand of subcommands) {
+        await spawnCommand(subcommand, i);
+    }
+}
+
